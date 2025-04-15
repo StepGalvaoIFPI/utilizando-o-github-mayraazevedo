46ba21993be0f3e9a89606371b955ae75adf937c
mayraazevedo <azevedo_mayra@hotmail.com>
2025-04-15 20:29:05 -0300

    Update q3.py
    Referente a atividade 01

--- respostas/q3.py
+++ respostas/q3.py
@@ -1,1 +1,9 @@
 #Crie um programa que exiba as seguintes informações, cada uma em uma linha:
+nome_completo = "Mayra Glayce Azevedo Sobreira"
+idade = 24
+cidade_natal = "Teresina"
+estado_nascimento = "Piauí"
+print(nome_completo)
+print(idade)
+print(cidade_natal)
+print(estado_nascimento)
